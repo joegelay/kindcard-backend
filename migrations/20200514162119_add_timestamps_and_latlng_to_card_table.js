a6fbb1173160b907f8632c4d48903dbb9697b477
joegelay <joegelay@gmail.com>
2020-05-14 16:40:50 -0600

    timestamp created_at defaults to current time

--- migrations/20200514162119_add_timestamps_and_latlng_to_card_table.js
+++ migrations/20200514162119_add_timestamps_and_latlng_to_card_table.js
@@ -2,7 +2,7 @@ exports.up = async function(knex) {
     await knex.schema.table("card", table => {
         table.float("lat")
         table.float("lng")
-        table.timestamps()
+        table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'))
   })
 };
 
@@ -10,5 +10,7 @@ exports.down = async function(knex) {
     await knex.schema.table("card", table => {
         table.dropColumn("lat")
         table.dropColumn("lng")
+        table.dropColumn("created_at")
+        table.dropColumn("updated_at")
   })
 };
