027ad792a6d573b189cf9ca581be0411938acde6
joegelay <joegelay@gmail.com>
2020-05-26 19:38:50 -0600

    create production db

--- knexfile.js
+++ knexfile.js
@@ -3,4 +3,13 @@ module.exports = {
     client: 'pg',
     connection: "postgresql:///kindcardobjection"
   },
+  production: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: process.env.PRODUCTIONUSER,
+      password: process.env.PRODUCTIONPASS,
+      database: process.env.PRODUCTIONDB,
+    }
+  },
 };
